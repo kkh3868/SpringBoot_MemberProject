863ffbe0e0716e526dfcd3772ccefccacc1b669e
GeonHui Kim <rjsgml198@naver.com>
2024-03-03 17:06:09 +0900

    Changes

--- src/main/java/com/study/member/controller/MemberController.java
+++ src/main/java/com/study/member/controller/MemberController.java
@@ -6,10 +6,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,43 +18,88 @@ public class MemberController {
 
     // 회원가입 페이지 출력 요청
     @GetMapping("/member/save")
-    public String saveForm(){
+    public String saveForm() {
         return "save";
     }
 
     // RequestParam : http에서 memberEmail 등에 저장된 값을 String 문자열에 담는다
     // ModelAttribute : http에서 post로 보내주는 것들을 DTO에 있는 필드와 이름이 같다면 필드에 자동으로 담는다
     @PostMapping("/member/save")
-    public String save(@ModelAttribute MemberDTO memberDTO){
+    public String save(@ModelAttribute MemberDTO memberDTO) {
         System.out.println("MemberController.save");
         System.out.println("memberDTO = " + memberDTO);
         memberService.save(memberDTO);
         return "index";
     }
+
     @GetMapping("/member/login")
-    public String loginForm(){
+    public String loginForm() {
         return "login";
     }
+
     @PostMapping("/member/login")
-    public String loginForm(@ModelAttribute MemberDTO memberDTO, HttpSession session){
+    public String loginForm(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
         MemberDTO loginResult = memberService.login(memberDTO);
-        if(loginResult != null){
+        if (loginResult != null) {
             // login 성공
-            session.setAttribute("loginEmail",loginResult.getMemberEmail());
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
             return "main";
-        }else{
+        } else {
             // login 실패
             return "login";
         }
     }
 
     @GetMapping("/member/")
-    public String findAll(Model model){
+    public String findAll(Model model) {
         List<MemberDTO> memberDTOList = memberService.findAll();
         // 어떠한 html로 가져갈 데이터가 있다면 model사용
-        model.addAttribute("memberList",memberDTOList);
+        model.addAttribute("memberList", memberDTOList);
         return "list";
     }
 
+    // @PathVariable은 주소로 넘어온 {id}를 매개변수 id에 담겠다는 의미임
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable("id") Long id, Model model) {
+        MemberDTO memberDTO = memberService.findById(id);
+        model.addAttribute("member", memberDTO);
+        return "detail";
+    }
 
+    @GetMapping("/member/update")
+    public String updateForm(HttpSession session, Model model) {
+        String myEmail = (String) session.getAttribute("loginEmail");
+        MemberDTO memberDTO = memberService.updateForm(myEmail);
+        model.addAttribute("updateMember", memberDTO);
+        return "update";
+    }
+
+    @PostMapping("/member/update")
+    public String update(@ModelAttribute MemberDTO memberDTO) {
+        memberService.update(memberDTO);
+        return "redirect:/member/" + memberDTO.getId();
+    }
+    @GetMapping("/member/delete/{id}")
+    public String deletyById(@PathVariable("id") Long id){
+        memberService.deleteById(id);
+        return "redirect:/member/";
+    }
+    @GetMapping("/member/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "index";
+    }
+    // ajax쓸 때는, @ResponseBody 꼭 사용해야함
+    // RequestParam은, Post방식으로 보내주는 변수를 매개변수로 쓰겠다고 선언하는 것임
+    @PostMapping("/member/email-check")
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail){
+        System.out.println("memberEmail = " + memberEmail);
+        String checkResult = memberService.emailCheck(memberEmail);
+        if(checkResult != null){
+            return checkResult;
+        }else{
+            return "no";
+        }
+
+    }
 }
--- src/main/java/com/study/member/service/MemberService.java
+++ src/main/java/com/study/member/service/MemberService.java
@@ -6,6 +6,7 @@ import com.study.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -54,5 +55,45 @@ public class MemberService {
             memberDTOList.add(MemberDTO.toMemberDTO(memberEntity));
         }
         return memberDTOList;
+    }
+    public MemberDTO findById(Long id) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(id);
+        if (optionalMemberEntity.isPresent()) {
+//            MemberEntity memberEntity = optionalMemberEntity.get();
+//            MemberDTO memberDTO = MemberDTO.toMemberDTO(memberEntity);
+//            return memberDTO;
+            return MemberDTO.toMemberDTO(optionalMemberEntity.get());
+        } else {
+            return null;
+        }
+
+    }
+
+    public MemberDTO updateForm(String myEmail) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findByMemberEmail(myEmail);
+        if (optionalMemberEntity.isPresent()) {
+            return MemberDTO.toMemberDTO(optionalMemberEntity.get());
+        } else {
+            return null;
+        }
+    }
+
+    public void update(MemberDTO memberDTO) {
+        memberRepository.save(MemberEntity.toUpdateMemberEntity(memberDTO));
+    }
+
+    public void deleteById(Long id) {
+        memberRepository.deleteById(id);
+    }
+
+    public String emailCheck(String memberEmail) {
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberEmail);
+        if(byMemberEmail.isPresent()){
+            // 조회결과가 있다 -> 사용할 수 없다.
+            return null;
+        }else{
+            // 조회결과가 없다 -> 사용할 수 있다.
+            return "ok";
+        }
     }
 }
